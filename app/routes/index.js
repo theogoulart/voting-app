7642dbc18ff251dd2b04586d79be4021f102f768
Theo Goulart <theo_goulart@hotmail.com>
2018-05-01 02:08:21 +0000

    refactor index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,7 +16,7 @@ module.exports = function (app, passport) {
 	var clickHandler = new ClickHandler();
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
