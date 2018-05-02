38645078945f182492d63b6506e4afe60dc6c061
theogoulart <theo_goulart@hotmail.com>
2018-05-02 02:39:04 +0000

    remove routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,7 +9,7 @@ module.exports = function (app, passport) {
 		if (req.isAuthenticated()) {
 			return next();
 		} else {
-			res.redirect('/login');
+			res.redirect('/');
 		}
 	}
 
@@ -17,29 +17,18 @@ module.exports = function (app, passport) {
 
 	app.route('/')
 		.get(function (req, res) {
-			res.render('index');
-			//res.sendFile(path + '/public/index.html');
+			res.render('index', {user: req.user});
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+	app.route('/create')
+		.get(isLoggedIn, function (req, res) {
+			res.render('create', {user: req.user});
 		});
 
 	app.route('/logout')
 		.get(function (req, res) {
 			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
+			res.redirect('/');
 		});
 
 	app.route('/auth/github')
